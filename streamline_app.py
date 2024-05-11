2e5617968ba3ee0dc97840c75717f972806fbf4d
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-10 23:11:39 -0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,21 @@
+#######################
+# Import libraries
 import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+
 st.write("""
       
       json
