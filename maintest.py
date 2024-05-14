fc1c5f94e9d36b8238061b252bc453932af081e8
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-14 01:03:47 -0500

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -12,7 +12,7 @@ from censustools import *
 def checkCensusUpdate():
     #Once the cron job is launched, this function check new census release for the month of June and create a CSV accordingly 
     api1=API('github')
-    #single_file_generator_01(api1)
+    single_file_generator_01(api1)
     #merge_02(api1)
     #group_by_03(api1) 
 
