8367745c98c358b16e3ca4c82d2bde714fbb6cff
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-13 23:02:46 -0500

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -1,12 +1,29 @@
-from datetime import datetime
+import requests
+import os
+import pandas as pd
+import regex as re
+import time
+from datetime import datetime as dt
+import json
+import logging
+import plotly.express as px
+from censustools import *
 
-def main():
+def checkCensusUpdate():
+    api2=API('github')
+    single_file_generator_01(api2)
+    merge_02(api2)
+    group_by_03(api2)
+    api2.df = readCSV_and_fuse_dfs_04(api2)
+
+def store_date():
     ourfile = "testhour.txt" 
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = dt.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(ourfile, 'a') as file:
         file.write(current_time + '\n')
       
     print(f"Current time written {current_time} in the file {ourfile}")
 
 if __name__ == '__main__':
-    main()
+    store_date()
+    checkCensusUpdate()
