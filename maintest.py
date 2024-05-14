d4af2e4827e000c07f55d3f0228e988813ae35c1
w86w86 <54444342+w86w86@users.noreply.github.com>
2024-05-14 00:25:49 -0500

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -12,17 +12,16 @@ from censustools import *
 def checkCensusUpdate():
     #Once the cron job is launched, this function check new census release for the month of June and create a CSV accordingly 
     api1=API('github')
-    single_file_generator_01(api1)
-    merge_02(api1)
-    group_by_03(api1) 
+    #single_file_generator_01(api1)
+    #merge_02(api1)
+    #group_by_03(api1) 
 
 def store_date():
     ourfile = "testhour.txt" 
     current_time = dt.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(ourfile, 'a') as file:
-        file.write(current_time + '\n')
-      
-    print(f"The update on the Census.gov data ran at {current_time} in the file {ourfile}")
+         msg = f"The update on the Census.gov data ran at {current_time} in the file {ourfile}")
+        file.write(msg)
 
 if __name__ == '__main__':
     store_date()
